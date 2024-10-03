72ea96dc71683bbd9afdec12fd3f009c79126dd7
ClaraCrazy <clara@crazyco.xyz>
2024-10-03 02:14:47 +0200

    Update example config & surroundings

--- application/config.sample.js
+++ application/config.sample.js
@@ -1,27 +1,27 @@
 const config = {
 	email: {
-		domains: process.env.EMAIL_DOMAINS,
-		purgeTime: process.env.PURGE_TIME || {
-			time: 48,
+		domains: process.env.EMAIL_DOMAINS, // List object of domains
+		purgeTime: process.env.EMAIL_PURGE_TIME || {
+			time: 48, // Time value to purge
 			unit: 'hours', // minutes, hours, days
 			convert: true, // Convert to highest sensible unit
 		}
 	},
 	imap: {
-		user: process.env.IMAP_USER,
-		password: process.env.IMAP_PASSWORD,
-		host: process.env.IMAP_SERVER,
-		port: process.env.IMAP_PORT || 993,
-		tls: process.env.IMAP_TLS || true,
-		authTimeout: process.env.IMAP_AUTHTIMEOUT || 3000,
-		refreshIntervalSeconds: process.env.IMAP_REFRESH_INTERVAL_SECONDS || 60
+		user: process.env.IMAP_USER, // imap user
+		password: process.env.IMAP_PASSWORD, // imap password
+		host: process.env.IMAP_SERVER, // imap server
+		port: process.env.IMAP_PORT || 993, // imap port
+		tls: process.env.IMAP_TLS || true, // use secure connection?
+		authTimeout: process.env.IMAP_AUTHTIMEOUT || 3000, // timeout for auth
+		refreshIntervalSeconds: process.env.IMAP_REFRESH_INTERVAL_SECONDS || 60 // refresh interval
 	},
 	http: {
-		port: normalizePort(process.env.HTTP_PORT || 3000),
-		branding: process.env.HTTP_BRANDING || ["48hr.email", "CrazyCo", "https://crazyco.xyz"],
-		examples: process.env.HTTP_EXAMPLES || {
-			email: "example@48hr.email",
-			ids: [1, 2, 3]
+		port: normalizePort(process.env.HTTP_PORT || 3000), // http port
+		branding: process.env.HTTP_BRANDING || ["48hr.email", "CrazyCo", "https://crazyco.xyz"], // branding
+		examples: process.env.HTTP_EXAMPLES || { 
+			email: "example@48hr.email", // example email
+			ids: [1, 2, 3] // example ids
 		}
 	},
 }
